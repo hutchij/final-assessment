58413c221638d042eca1fff29bc61b34338276cd
john hutchinson <john.hutchinson@autodesk.com>
2016-08-23 16:01:59 -0700

    finished

--- public/module.js
+++ public/module.js
@@ -8,6 +8,182 @@ App.run(function($rootScope) {
 
 });
 
+//I could not get this to work
+// App.config(['$routeProvider', '$locationProvider',
+//   function($routeProvider, $locationProvider) {
+//     $routeProvider
+//       .when('/home', {
+//         templateUrl: 'book.html',
+//         controller: 'GiffyListCtrl'
+//       })
+//       .when('/login', {
+//         templateUrl: 'login.html'
+//       });
+
+//     $locationProvider.html5Mode(true);
+// }])
+
+//simple controller for login
+App.controller('LoginCtrl',['$scope', '$http', function($scope, $http){
+
+    $scope.login = function() {
+        console.log('GiffyListCtrl select called with gif: ', gif);
+        //not yet implemented, should call out to GitHub which is currently just done from HTML
+
+    }
+
+}])
+
+//define a controller for the giffy brwoser list
+App.controller('GiffyListCtrl',['$scope', '$http', function($scope, $http){
+
+	//some scope variable to hold the restuls
+	//$scope.giflist = fakeData;
+	$scope.giflist = [];
+	$scope.selected = [];
+
+	//more scope to manage favorites
+	$scope.privGiflist = [];
+	$scope.privSelected = [];
+
+
+	//controller initialization 
+	//1) load gifs from the internet
+	//2) load private gifs
+
+	//populate the data on load
+	//http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
+
+    $http({
+    method: 'GET',
+    url: "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC",
+    headers: {'Accept': 'application/json'}})
+    .then(function(response) {
+        //success
+        console.log('successfully fetch giffy data', response.data.data)
+        $scope.giflist = response.data.data;
+    }, function(response) {
+        console.log('error fetching giffy data', response)
+        $scope.giflist = [];
+    });
+
+   
+
+    //popluate favorites
+
+	//call our own backend to get the GIF data
+    $http({
+    method: 'GET',
+    url: "http://api/favorites",
+    headers: {'Accept': 'application/json'}})
+    .then(function(response) {
+        //success
+        console.log('successfully fetch giffy data', response.data.data)
+        $scope.privGiflist = response.data.data;
+        $scope.privSelected = [];
+    }, function(response) {
+        console.log('error fetching giffy data', response)
+        $scope.privGiflist = [];
+        $scope.privSelected = [];
+   });
+
+
+    //when the user select a public GIF
+     $scope.selectToSave = function(gif) {
+        console.log('GiffyListCtrl select called with gif: ', gif)
+        $scope.selected.push(angular.copy(gif))
+    }
+
+    //when the user selects a favorite to remove
+     $scope.selectToDelete = function(gif) {
+        console.log('GiffyListCtrl select called with gif: ', gif)
+        $scope.privSelected.push(angular.copy(gif))
+    }
+
+
+    //save favorites to teh backend
+    $scope.saveFavorites = function() {
+        console.log('GiffyListCtrl save called.')
+
+        //persist this to the backend
+        $http({
+        method: 'POST',
+        url: "/api/favorites",
+        data: angular.copy($scope.selected),
+        headers: {'Content-Type': 'application/json'}})
+        .then(function(response) {
+            //First function handles success
+         $scope.selected = [];
+         $scope.privSelected = [];
+         $scope.privGiflist = response.data;
+       }, function(response) {
+            //Second function handles error
+            $scope.selected = [];
+         $scope.privSelected = [];
+         $scope.privGiflist = [];
+       });
+
+
+        //clear the selection
+        $scope.selected = [];
+
+    }
+
+
+
+    //push the selection to teh backend
+    //TODO remove a selection from storage
+    $scope.deleteFavorites = function() {
+        console.log('GiffyListCtrl save called.')
+
+        //persist this to the backend
+        $http({
+        method: 'DELETE',
+        url: "/api/favorites",
+        data: angular.copy($scope.privSelected),
+        headers: {'Content-Type': 'application/json'}})
+        .then(function(response) {
+            //First function handles success
+            $scope.privSelected = [];
+            $scope.privGiflist = response.data;
+        }, function(response) {
+            //Second function handles error
+            $scope.privSelected = [];
+            $scope.privGiflist = [];
+        });
+
+
+        //clear the selection
+        $scope.privSelected = [];
+
+    }
+
+
+
+
+
+
+
+    //a method to remove from storage
+    //TODO
+    $scope.deleteFavorites = function(gif) {
+        console.log('PrivateGifsCtrl select called with gif: ', gif)
+        $scope.selected.push(angular.copy(gif))
+    }
+}])
+
+
+//a controller to manage favirote gifs saved to the server
+App.controller('PrivateGifsCtrl',['$scope', '$http', function($scope, $http){
+
+
+
+
+}])
+
+
+//data for testing below
+
 var fakeData =   [
     {
       "type": "gif",
@@ -69,26 +245,3 @@ var fakeData =   [
         }
         }
     }];   
-
-//define a controller for the giffy brwoser list
-App.controller('GiffyListCtrl',['$scope', '$http', function($scope, $http){
-
-	//some scope variable to hold the restuls
-	$scope.giflist = fakeData;
-	$scope.selected = [];
-
-	//populate the data on load
-	//http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC
-
-    $scope.select = function(gif) {
-        console.log('GiffyListCtrl select called with gif: ', gif)
-        $scope.selected.push(angular.copy(gif))
-    }
-
-    $scope.save = function() {
-        console.log('GiffyListCtrl save called.')
-
-        //persist this to the backend
-
-    }
-}])
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,130 @@
 var express = require('express');
+var request = require('request')
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/finalproject';
+
+
+function restrict(req, res, next) {
+    if (req.session.access_token) {
+        next();
+    } else {
+        req.session.error = 'Access denied!';
+        res.redirect('/login');
+    }
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/index.html')
+});
+router.get('/login', function(req, res) {
+    //   res.redirect('login');
+    res.render('login.html')
+});
+router.get('/home', function(req, res) {
+    //   res.redirect('login');
+    res.render('home.html')
+});
+
+
+//some db helpers, should be factored out of here
+
+var insertRecords = function(favs, db, callback) {
+	// Get the documents collection
+	var collection = db.collection('favorites');
+	// Insert some documents
+	collection.insert(favs, function(err, result) {
+		assert.equal(err, null);
+		console.log("Inserted user to DB");
+		return callback(err, result);
+	});
+}
+
+
+
+var findRecords = function(db, callback) {
+	var collection = db.collection('favorites');
+
+	collection.find({}).toArray(function(err, result) {
+		console.log('retrieved restult', result)
+		assert.equal(err, null);
+		return callback(err, result)
+	})
+}
+
+router.post('/api/favorites', function(req, res) {
+	console.log('in api post /favorites', req.body)
+		// handleAddUser(req.body, function(err, results){
+		// 	assert(null, err);
+		// 	res.json(results)
+		// })
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected correctly to server");
+
+		insertRecords(req.body, db, function(err, result) {
+			findRecords(db, function(err, result) {
+				db.close();
+				res.json(result);
+			})
+		});
+	});
+
+});
+
+router.get('/api/auth', function(req, res, next) {
+		console.log('in auth callback')
+
+		//exchange for authcode
+
+		var auth_code = req.query['code'];
+		console.log(auth_code);
+		var redirect_uri = 'http://localhost:3000/api/auth'
+		var client_id = 'abe797dd0bcf8911c15a';
+		var client_secret = 'c81727a569562b9089c8a8ccd8499f958a0b666f';
+
+		var validatequery = 'client_id=' + client_id + '&client_secret=' + client_secret + '&code=' + auth_code + '&redirect_uri=' + redirect_uri;
+
+
+		request.post({
+				url: 'https://github.com/login/oauth/access_token',
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Accept': 'application/json'
+				},
+				body: validatequery
+			},
+			function(err, postResp, body) {
+				if (err) {
+					console.log('validate token error' + err)
+					return;
+				} else if (postResp.statusCode != 200) {
+					console.log('Could not validate token' + postResp.statusCode)
+						//console.log(validatequery);
+						//console.log(encodeURIComponent(validatequery));
+				} else {
+					console.log('validate token success', body);
+					var bodyResp = JSON.parse(body)
+					console.log('response object', bodyResp);
+					var token = bodyResp['access_token'];
+					//var userid = bodyResp['access_token'].userid;
+					console.log('access token', token)
+
+					req.session.regenerate(function() {
+						req.session.access_token = token;
+						//res.redirect('/');
+						res.redirect('/');
+					});
+
+				}
+			}//anon
+		)//post
 });
 
 module.exports = router;
